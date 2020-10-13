c20f8d09763fe657b9239f7a2e6bfb8dc780600d
Scott Fabini <scott.fabini@gmail.com>
2020-10-12 21:37:39 -0700

    Improve StarRating

--- src/StarRating.js
+++ src/StarRating.js
@@ -6,13 +6,20 @@ import { useState } from "react";
 export default function StarRating({ totalStars = 5 }) {
   const [selectedStars, setSelectedStars] = useState(3);
 
-  return [...Array(totalStars)].map((n, i) => (
-    <Star
-      key={i}
-      selected={i < selectedStars}
-      onSelect={() => setSelectedStars(i + 1)}
-    />
-  ));
+  return (
+    <>
+      {[...Array(totalStars)].map((n, i) => (
+        <Star
+          key={i}
+          selected={i < selectedStars}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
 }
 
 // Below is all using props, versus above using state/hooks
